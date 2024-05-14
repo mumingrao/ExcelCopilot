9e481e43ecf35b8323e6d3ed2aea43ad61825bf2
raomuming <raomuming@foxmail.com>
2024-05-14 18:35:16 +0800

    request

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -14,17 +14,8 @@ Component({
     // 事件处理函数
     askCopilot() {
       console.log("ask copilot");
-      const answer = {
-        exist: true,
-        formula: '=A1+B1',
-        description: '在Excel中，如果你想要将两列相加，你可以使用加号(+)来连接两个单元格引用。例如，如果你想将A列和B列的第一行相加，这个公式会将A1单元格的值和B1单元格的值相加，并将结果放在C1单元格中。'
-      };
-      this.setData({
-        answer: answer,
-      })
-      /**
       wx.request({
-        url: 'http://localhost:5566/v1/formula',
+        url: 'https://dogmotto.com/v1/formula',
         data: {
           prompt: this.data.prompt,
         },
@@ -34,15 +25,23 @@ Component({
         },
         success: res => {
           console.log('success =>', res);
+          let data = res.data;
+          this.setData({
+            answer: data,
+          });
         },
         fail: res => {
           console.log("fail =>", res);
+          this.setData({
+            answer: {
+              description: "网络错误,请稍后再试.",
+            }
+          })
         },
         complete: res => {
           console.log("complete =>", res);
         }
       })
-      */
     },
     handlePromptInput(options: any) {
       let value = options.detail.value;
